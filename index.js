b0f215f1e6886dca20f721efb469ab2dcdd4688b
Mason Demerais <mason.demerais@usask.ca>
2020-05-04 12:10:06 -0600

    Setup socketio

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
+
+
+io.on('connection', socket => {
+    console.log(`${socket.id} has connected.`)
+})
+
+io.on('disconnect', socket => {
+    console.log(`${socket.id} has disconnected.`)
+})
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
