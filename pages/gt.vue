dfc9e28656f44d4b527651da756447e8ed2b8c0f
Your Name <you@example.com>
2020-05-22 20:26:11 +0000

    Test

--- pages/gt.vue
+++ pages/gt.vue
@@ -6,9 +6,11 @@
 /* eslint-disable no-unused-vars */
 import cytoscape from 'cytoscape'
 import consola from 'consola'
+import GT from '~/utils/GT'
 
 export default {
   mounted () {
+    const gt = new GT()
     const cy = cytoscape({
 
       container: document.getElementById('cy'), // container to render in
@@ -54,17 +56,27 @@ export default {
     })
 
     const eles = cy.add([
-      { group: 'nodes', data: { id: 'n0' }, position: { x: 100, y: 100 } },
-      { group: 'nodes', data: { id: 'n1' }, position: { x: 200, y: 200 } },
+      { group: 'nodes', data: { id: 'n0' }, position: { x: 0, y: 0 } },
+      { group: 'nodes', data: { id: 'n1' }, position: { x: 0, y: 0 } },
       { group: 'edges', data: { id: 'e0', source: 'n0', target: 'n1' } }
     ])
 
-    cy.add({ group: 'nodes', data: { id: 'n3', weight: 1 }, position: { x: 150, y: 100 } })
-    cy.add({ group: 'nodes', data: { id: 'n4', weight: 51 }, position: { x: 150, y: 150 } })
+    cy.add({ group: 'nodes', data: { id: 'n3', weight: 1 }, position: { x: 0, y: 0 } })
+    cy.add({ group: 'nodes', data: { id: 'n4', weight: 51 }, position: { x: 0, y: 0 } })
 
     cy.remove('node[weight > 50]') // remove nodes with weight greater than 50
 
     cy.nodes().on('click', e => consola.log('clicked', e))
+    // cy.nodes().on('position', e => consola.log('position ', e))
+    cy.nodes().position({ x: 0, y: 0 })
+
+    const layout = cy.layout({ name: 'cose' })
+
+    layout.run()
+
+    consola.log(cy.elements().jsons())
+
+    gt.connect('gt')
   }
 }
 </script>
