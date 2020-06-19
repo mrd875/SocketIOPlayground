f80708f6def4a6969d0ace01ed6746c6ed9fb6d3
Your Name <you@example.com>
2020-06-19 23:45:47 +0000

    Added id authentication, leaving room, better promises

--- src/GT.js
+++ src/GT.js
@@ -1,6 +1,7 @@
 /* eslint-disable require-await */
 const EventEmitter = require('./EventEmitter')
 const io = require('socket.io-client/dist/socket.io')
+const _ = require('lodash')
 
 /**
  * Fired when the server sends us the entire state of the room.
@@ -41,6 +42,13 @@ const io = require('socket.io-client/dist/socket.io')
 * @event GT#disconnect
 * @param {String} reason
 */
+
+/**
+ * Fires when we receive a connection error when trying to connect to the server.
+ *
+ * @event GT#connect_error
+ * @param {Object} error
+ */
 
 /**
  * Fires when we receive a connection error when trying to connect to the server.
@@ -94,9 +102,10 @@ const io = require('socket.io-client/dist/socket.io')
  * @emits GT#connect
  * @emits GT#disconnect
  * @emits GT#connect_error
+ * @emits GT#error
  *
- * @property {Object} socket The interal socket we use as communation.
- * @property {String} id Our unique identifier, issued from the server, undefined when we are not connected to the server.
+ * @property {Object} socket The internal socket we use as communication.
+ * @property {String} id Our unique identifier, undefined when we are not connected to the server.
  */
 class GT extends EventEmitter {
   /**
@@ -113,14 +122,12 @@ class GT extends EventEmitter {
 
     this.socket = socket
 
-    // setup the events.
-    socket.on('init_state', (state, users, room) => {
-      this.emit('init_state', state, users, room)
-    })
-
+    // when someone joins a room, including us
     socket.on('connected', (id, user) => {
       this.emit('connected', id, user)
     })
+
+    // when someone leaves the room
     socket.on('disconnected', (id, reason) => {
       this.emit('disconnected', id, reason)
     })
@@ -140,53 +147,161 @@ class GT extends EventEmitter {
     })
 
     this.id = undefined
+    this.room = undefined
 
-    socket.on('connect', () => {
-      this.id = socket.id
-
-      this.emit('connect', socket.id)
-    })
-
+    // when we disconnect from the server
     socket.on('disconnect', (reason) => {
       this.emit('disconnect', reason)
 
       this.id = undefined
+      this.room = undefined
     })
+
+    // when we join a room
+    socket.on('joined', (room, roomState, users) => {
+      this.room = room
+
+      this.emit('joined', room, roomState, users)
+    })
+
+    // when we auth.
+    socket.on('authed', (authPayload) => {
+      this.id = authPayload.id
+
+      this.emit('authed', authPayload.id, authPayload.state)
+    })
+
+    // when any error is thrown
+    socket.on('error', (err) => {
+      this.emit('error', err)
+    })
+
+    // when a connect error is thrown
+    socket.on('connect_error', (err) => {
+      this.emit('connect_error', err)
+    })
+
+    // when we connect to the server
+    socket.on('connect', (socketId) => {
+      this.emit('connect', socketId)
+    })
+
+    // when we were removed form a room
+    socket.on('leftroom', (reason) => {
+      this.room = undefined
+      this.emit('leftroom', reason)
+    })
+
+    this.removeObjectsWithNull = obj => {
+      return _(obj)
+        .pickBy(_.isObject) // get only objects
+        .mapValues(this.removeObjectsWithNull) // call only for values as objects
+        .assign(_.omitBy(obj, _.isObject)) // save back result that is not object
+        .omitBy(_.isNil) // remove null and undefined from object
+        .value() // get value
+    }
   }
 
   /**
      * @returns {Boolean} If we are connected to the server.
      */
   isConnected () {
+    return this.socket.connected
+  }
+
+  /**
+     * @returns {Boolean} If we are authenticated
+     */
+  isAuthed () {
     return this.id !== undefined
+  }
+
+  /**
+     * @returns {Boolean} If we are in a room
+     */
+  isInRoom () {
+    return this.room !== undefined
   }
 
   /**
      * @async
      * Connects to the server and joins a room, returns when we connect successfully.
      * @throws Error when cannot connect for some reason.
+     * @param {String} id The id we want to assign and auth ourselves as.
      * @param {String} room The roomname we want to join.
      * @param {Object} userPayload An optional initial state we have as a user.
      */
-  connect (room, userPayload) {
+  connect () {
     return new Promise((resolve, reject) => {
       if (this.isConnected()) { reject(new Error('We are already connected.')) }
 
-      this.socket.connect()
+      let handleConnect
+      let handleConnectError
+      this.socket.once('connect', handleConnect = (socketId) => {
+        this.socket.off('connect_error', handleConnectError)
 
-      const throwErr = (err) => {
-        this.socket.off('connect', connect)
+        resolve(socketId)
+      })
+      this.socket.once('connect_error', handleConnectError = (err) => {
+        this.socket.off('connect', handleConnect)
+
         reject(err)
-      }
-      this.socket.once('connect_error', throwErr)
+      })
 
-      const connect = () => {
-        this.socket.off('connect_error', throwErr)
-        this.socket.emit('join_room', room, userPayload)
+      this.socket.connect()
+    })
+  }
 
-        resolve(this)
-      }
-      this.socket.once('connect', connect)
+  auth (id) {
+    return new Promise((resolve, reject) => {
+      if (!this.isConnected()) { reject(new Error('We need to be connected.')) }
+      if (this.isAuthed()) { reject(new Error('We already authed')) }
+
+      let handleAuth
+      let handleAuthError
+      this.socket.once('auth', handleAuth = (authPayload) => {
+        this.socket.off('error', handleAuthError)
+
+        resolve(authPayload)
+      })
+      this.socket.on('error', handleAuthError = (err) => {
+        if (err.type !== 'auth') return
+
+        this.socket.off('error', handleAuthError)
+        this.socket.off('auth', handleAuth)
+
+        reject(err)
+      })
+
+      this.socket.emit('auth', {
+        id
+      })
+    })
+  }
+
+  join (room, userPayload) {
+    return new Promise((resolve, reject) => {
+      if (!this.isConnected()) { reject(new Error('We need to be connected.')) }
+      if (!this.isAuthed()) { reject(new Error('We need to be authed.')) }
+      if (this.isInRoom()) { reject(new Error('We Already in room')) }
+
+      let handleJoin
+      let handleJoinError
+      this.socket.once('joined', handleJoin = (room, roomState, users) => {
+        this.socket.off('error', handleJoinError)
+
+        resolve(room, roomState, users)
+      })
+      this.socket.on('error', handleJoinError = (err) => {
+        if (err.type !== 'join') return
+
+        this.socket.off('error', handleJoinError)
+        this.socket.off('auth', handleJoin)
+
+        reject(err)
+      })
+
+      this.socket.emit('join', { room }, userPayload)
     })
   }
 
@@ -194,11 +309,39 @@ class GT extends EventEmitter {
      * Disconnects from the server.
      */
   disconnect () {
-    if (!this.isConnected()) { return this }
+    return new Promise((resolve, reject) => {
+      if (!this.isConnected()) { reject(new Error('We already disconnected')) }
 
-    this.socket.disconnect()
+      this.socket.once('disconnect', () => {
+        resolve()
+      })
 
-    return this
+      this.socket.disconnect()
+    })
+  }
+
+  leaveRoom () {
+    return new Promise((resolve, reject) => {
+      if (!this.isInRoom()) { reject(new Error('We need to be in a room')) }
+
+      let handleLeave
+      let handleLeaveError
+      this.socket.once('leftroom', handleLeave = (reason) => {
+        this.socket.off('error', handleLeaveError)
+
+        resolve(reason)
+      })
+      this.socket.on('error', handleLeaveError = (err) => {
+        if (err.type !== 'leaveroom') return
+
+        this.socket.off('error', handleLeaveError)
+        this.socket.off('leftroom', handleLeave)
+
+        reject(err)
+      })
+
+      this.socket.emit('leaveroom')
+    })
   }
 
   /**
@@ -206,6 +349,7 @@ class GT extends EventEmitter {
      * @param {Object} payloadDelta The delta object to send to the server.
      */
   updateStateReliable (payloadDelta) {
+    if (!this.isInRoom()) { throw new Error('Need to be in a room') }
     this.socket.emit('state_updated_reliable', payloadDelta)
     return this
   }
@@ -215,6 +359,7 @@ class GT extends EventEmitter {
      * @param {Object} payloadDelta The delta object to send to the server.
      */
   updateStateUnreliable (payloadDelta) {
+    if (!this.isInRoom()) { throw new Error('Need to be in a room') }
     this.socket.emit('state_updated_unreliable', payloadDelta)
     return this
   }
@@ -224,6 +369,7 @@ class GT extends EventEmitter {
      * @param {Object} payloadDelta The delta object to send to the server.
      */
   updateUserReliable (payloadDelta) {
+    if (!this.isInRoom()) { throw new Error('Need to be in a room') }
     this.socket.emit('user_updated_reliable', payloadDelta)
     return this
   }
@@ -233,6 +379,7 @@ class GT extends EventEmitter {
      * @param {Object} payloadDelta The delta object to send to the server.
      */
   updateUserUnreliable (payloadDelta) {
+    if (!this.isInRoom()) { throw new Error('Need to be in a room') }
     this.socket.emit('user_updated_unreliable', payloadDelta)
     return this
   }
