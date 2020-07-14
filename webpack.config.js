a2767e45de868ba861df2222e3fdab64da34b2e6
Your Name <you@example.com>
2020-07-14 21:21:51 +0000

    Removed babel for now, need to reconfigure

--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,6 @@ module.exports = [{
     library: 'GT',
     libraryTarget: 'umd',
     filename: 'gt.umd.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components|dist)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
   }
 },
 {
@@ -27,23 +13,5 @@ module.exports = [{
   output: {
     libraryTarget: 'commonjs2',
     filename: 'gt.cjs.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components|dist)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [['@babel/preset-env', {
-              modules: false,
-              useBuiltIns: 'usage',
-              targets: 'maintained node versions'
-            }]]
-          }
-        }
-      }
-    ]
   }
 }]
