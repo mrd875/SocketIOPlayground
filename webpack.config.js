3cbe9823d70cb5c5c08cdfb696e419c0ae4fb62d
Your Name <you@example.com>
2020-07-27 18:40:21 +0000

    REstored babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,25 @@ module.exports = [{
     library: 'GT',
     libraryTarget: 'umd',
     filename: 'gt.umd.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime',
+              '@babel/plugin-transform-modules-umd']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
   }
 },
 {
@@ -13,5 +32,24 @@ module.exports = [{
   output: {
     libraryTarget: 'commonjs2',
     filename: 'gt.cjs.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime',
+              '@babel/plugin-transform-modules-commonjs']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
   }
 }]
