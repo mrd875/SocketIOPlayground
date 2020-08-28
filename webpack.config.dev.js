a1df18b362c53a85061ef7986dbc72f42af00816
Your Name <you@example.com>
2020-08-28 18:28:13 +0000

    Fix dev cjs

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -9,11 +9,31 @@ module.exports = [{
   devtool: 'source-map'
 },
 {
-  name: 'cjs.dev',
+  name: 'cjs',
+  target: 'node',
   entry: './src/index.js',
   output: {
     libraryTarget: 'commonjs2',
     filename: 'gt.cjs.dev.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime',
+              '@babel/plugin-transform-modules-commonjs']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
   },
   devtool: 'source-map'
 }]
